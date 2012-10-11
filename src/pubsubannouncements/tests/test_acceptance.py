e390434347f6c216aaa073926f164cdf799e78f3
Asko Soukka <asko.soukka@iki.fi>
2012-10-11 11:37:35 +0300

    Removed unnecessary patch, due to fixes in c.zamqp test fixture.

--- src/pubsubannouncements/tests/test_acceptance.py
+++ src/pubsubannouncements/tests/test_acceptance.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
 """Acceptance testing using Robot Framework"""
 
-###
-# Monkeypatch http://bugs.python.org/issue14308 for Python 2.7.2
-# to kill AttributeError noise due to a bug in threading._DummyThread
-import threading
-
-
-def __stop(self):
-    # DummyThreads delete self.__block, but they have no waiters to
-    # notify anyway (join() is forbidden on them).
-    if not hasattr(self, '_Thread__block'):
-        return
-    self.__block.acquire()
-    self.__stopped = True
-    self.__block.notify_all()
-    self.__block.release()
-threading._DummyThread._Thread__stop = __stop
-##
-
 import unittest
 
 from plone.testing import layered
