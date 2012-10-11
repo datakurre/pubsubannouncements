11c8552949578d4d939ff4ec233196bdfa4e1d01
Asko Soukka <asko.soukka@iki.fi>
2012-10-11 07:48:05 +0300

    Added test for binding.

--- src/pubsubannouncements/tests/test_announcement.py
+++ src/pubsubannouncements/tests/test_announcement.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import unittest2 as unittest
 
+import re
+
 from collective.zamqp.testing import runAsyncTest
 
 from pubsubannouncements.testing import FUNCTIONAL_TESTING
@@ -17,3 +19,13 @@ class PubSubFunctional(unittest.TestCase):
 
     def testExchangeIsDeclared(self):
         runAsyncTest(self._testExchangeIsDeclared)
+
+    def _testAnonymousQueueIsDeclared(self):
+        rabbitctl = self.layer['rabbitctl']
+        bindings = [re.sub("amq.gen-.*", "amq.gen-", binding) for binding
+                    in rabbitctl('list_bindings')[0].split("\n")]
+        self.assertIn("announcements\texchange\tamq.gen-", bindings)
+
+    def testAnonymousQueueIsDeclared(self):
+        runAsyncTest(self._testAnonymousQueueIsDeclared)
+
